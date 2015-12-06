202e6874e72b4d1261db349478e78a9d9f295b66
김대석 <greatstone@satreci.com>
2015-12-07 03:45:16 +0900

    함수형으로 리팩토링

--- src/main/scala/gRanks.scala
+++ src/main/scala/gRanks.scala
@@ -2,13 +2,13 @@
 
 import java.net.URL
 
-import scala.collection.mutable
 import scala.io.Source
 
 /**
   * Created by greatstone on 2015. 11. 28..
   */
 object gRanks extends App {
+
   val filename = "A-large-practice"
   val writer = new java.io.PrintWriter(filename + ".out")
   val result = solve(getClass.getResource(s"gRanks/$filename.in"))
@@ -22,47 +22,43 @@ object gRanks extends App {
       val p = lines.next.toInt
       val s = lines.next.split(" ").map(_.toInt)
       val n = lines.next.toInt
-      val competitions = for (game <- 1 to n) yield lines.next.split(" ")
+      val competitions = for (game <- 1 to n) yield {
+        val competition = lines.next.split(" ")
+        (competition.head.toInt, competition.tail)
+      }
       val m = lines.next.toInt
 
       result += s"Case #$caseNum:\n"
-      result += determineRank(s, competitions, m)
+      result += determineRank(s, competitions, m).map(x => s"${x.rank}: ${x.name}\n").mkString
     }
     result.lines
   }
 
-  def determineRank(s: Array[Int], competitions: Seq[Array[String]], m: Int) = {
-    var scores = Map[String, mutable.MutableList[Int]]()
-    competitions.foreach { x =>
-      val w = x.head.toInt
-      for ((winner, index) <- x.tail.zipWithIndex) {
-        if (scores.contains(winner)) {
-          val score = w * s(index)
-          scores(winner) += score
-        }
-        else {
-          val score = w * s(index)
-          scores += (winner -> mutable.MutableList(score))
-        }
-      }
-    }
+  def determineRank(s: Array[Int], competitions: Seq[(Int, Array[String])], m: Int) = {
 
-    val totalScores = for ((athlete, score) <- scores.toArray) yield
+    val scores =
+      for ((w, winners) <- competitions;
+           (winner, index) <- winners.zipWithIndex)
+        yield (winner, w * s(index))
+    val scoreMap = scores.groupBy(_._1).mapValues(_.unzip._2)
+
+    val totalScores = for ((athlete, score) <- scoreMap.toArray) yield
       (athlete, score.sorted(Ordering[Int].reverse).take(m).sum)
 
     val rankTable = totalScores.sortWith((x, y) => (x._2 > y._2) || (x._2 == y._2 && x._1 < y._1))
+
     var rank = 1
     var previousScore = -1
-    var res = ""
-    for ((score, index) <- rankTable.zipWithIndex) {
+    for ((score, index) <- rankTable.zipWithIndex) yield {
       if (previousScore != score._2) {
         rank = index + 1
       }
       previousScore = score._2
-      res += s"$rank: ${score._1}\n"
+      Result(rank, score._1)
     }
-    res
   }
+
+  case class Result(rank: Int, name: String)
 
   writer.println(result)
   writer.close()
